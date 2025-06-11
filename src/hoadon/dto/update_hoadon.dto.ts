e893f58dcfb47c5f7833f821ca5756989e2c0952
Ductoan1812 <tOAN.TQ.HY.2003@GMAIL.COM>
2025-06-11 15:55:10 +0700

    Done BE HoaDon Hướng Dẫn Trong File TestAPIHoaDon.http

---
+++ src/hoadon/dto/update_hoadon.dto.ts
@@ -0,0 +1,28 @@
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class UpdateHoaDonDTO {
+  @IsInt()
+  SoDien: number;
+
+  @IsInt()
+  GiaDien: number;
+
+  @IsInt()
+  SoNuoc: number;
+
+  @IsInt()
+  GiaNuoc: number;
+
+  @IsInt()
+  GiaPhong: number;
+
+  @IsInt()
+  @IsOptional()
+  ChiPhiKhac?: number;
+
+  @IsString()
+  MaPhong: string;
+
+  @IsString()
+  MaNV: string;
+}
--- src/hoadon/hoadon.module.ts
+++ src/hoadon/hoadon.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HoadonController } from './hoadon.controller';
 import { HoadonService } from './hoadon.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HoaDon } from './hoadon.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([HoaDon])],
   controllers: [HoadonController],
-  providers: [HoadonService]
+  providers: [HoadonService],
 })
 export class HoadonModule {}
