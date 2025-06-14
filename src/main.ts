827ae3fa07dbdf082d4d93a1b3549400983c8da3
DucToan1812 <Toan.tq.hy.2003@gmail.com>
2025-06-14 02:54:24 -0700

    fix  app.enableCors(); đặt trước app.listent

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(process.env.PORT ?? 3000);
   app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
