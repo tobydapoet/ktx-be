83b42285979fce91de2dc86ea2845142585517bf
Viet Tung <tnkoko123@gmail.com>
2025-07-11 16:19:55 +0700

    fix role guard

--- src/account/roles.guard.ts
+++ src/account/roles.guard.ts
@@ -1,28 +1,17 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  Inject,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY } from './public.decorator';
 import { ROLES_KEY } from './roles.decorator';
-import { BlacklistService } from 'src/blacklist/blacklist.service';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-    private blacklistService: BlacklistService,
-  ) {}
+  constructor(private reflector: Reflector) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-
     if (isPublic) return true;
 
     const requiredRoles = this.reflector.getAllAndOverride<number[]>(
@@ -30,22 +19,10 @@ export class RolesGuard implements CanActivate {
       [context.getHandler(), context.getClass()],
     );
 
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    const authHeader = request.headers['authorization'];
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      throw new UnauthorizedException('Không có token');
-    }
-
-    const token = authHeader.replace('Bearer ', '');
-
-    const isBlacklisted = await this.blacklistService.isTokenBlacklisted(token);
-    if (isBlacklisted) {
-      throw new UnauthorizedException('Token đã bị thu hồi');
-    }
+    const { user } = context.switchToHttp().getRequest();
 
     if (!requiredRoles || requiredRoles.length === 0) return true;
+
     if (!user || user.ChucVu === undefined) return false;
 
     return requiredRoles.includes(user.ChucVu);
